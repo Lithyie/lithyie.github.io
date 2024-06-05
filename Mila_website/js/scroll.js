bb14822bee3e9037ba690d656adcffe4fe856b58
Lithy <lethofeur@gmail.com>
2024-06-05 16:17:31 +0200

    push

---
+++ Mila_website/js/scroll.js
@@ -0,0 +1,21 @@
+document.addEventListener('scroll', function() {
+    const section = document.querySelector('.innovation');
+    const sectionRect = section.getBoundingClientRect();
+    const windowHeight = window.innerHeight;
+
+    // Check if the section is in view
+    if (sectionRect.top <= windowHeight && sectionRect.bottom >= 0) {
+        section.classList.add('fixed');
+    } else {
+        section.classList.remove('fixed');
+    }
+});
+
+window.addEventListener('scroll', function() {
+    var nav = document.querySelector('.nav');
+    if (window.scrollY > 0) {
+        nav.classList.add('scrolled');
+    } else {
+        nav.classList.remove('scrolled');
+    }
+});
