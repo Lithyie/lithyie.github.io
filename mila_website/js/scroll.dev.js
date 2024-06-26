80c5ab4477768f46c7361f71af01615b6ff4fc6b
Lithy <lethofeur@gmail.com>
2024-06-26 09:37:32 +0200

    clean js

--- mila_website/js/scroll.dev.js
+++ mila_website/js/scroll.dev.js
@@ -1,16 +1,5 @@
 "use strict";
 
-// document.addEventListener('scroll', function() {
-//     const section = document.querySelector('.innovation');
-//     const sectionRect = section.getBoundingClientRect();
-//     const windowHeight = window.innerHeight;
-//     // Check if the section is in view
-//     if (sectionRect.top <= windowHeight && sectionRect.bottom >= 0) {
-//         section.classList.add('fixed');
-//     } else {
-//         section.classList.remove('fixed');
-//     }
-// });
 window.addEventListener('scroll', function () {
   var nav = document.querySelector('.nav');
 
