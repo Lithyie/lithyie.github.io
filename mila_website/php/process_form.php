070817877a8ac93302c6712e810b6fb723fcd599
Lithy <lethofeur@gmail.com>
2024-06-28 14:17:57 +0200

    clean annotations

--- mila_website/php/process_form.php
+++ mila_website/php/process_form.php
@@ -1,35 +1,30 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $to_email = "contact@luxantinnovation.com"; // Destination email address
-    $subject = "Nouveau message depuis le formulaire de contact"; // Email subject
+    $to_email = "contact@luxantinnovation.com";
+    $subject = "Nouveau message depuis le formulaire de contact";
 
     $name = filter_var(trim($_POST["name"]), FILTER_SANITIZE_STRING);
     $mail = filter_var(trim($_POST["mail"]), FILTER_SANITIZE_EMAIL);
     $message = filter_var(trim($_POST["message"]), FILTER_SANITIZE_STRING);
 
-    // Validate email
     if (!filter_var($mail, FILTER_VALIDATE_EMAIL)) {
         echo "Adresse e-mail invalide.";
         exit;
     }
 
-    // Check for email header injection
     if (preg_match("/[\r\n]/", $name) || preg_match("/[\r\n]/", $mail)) {
         echo "Tentative d'injection d'en-tête détectée.";
         exit;
     }
 
-    // Email body
     $email_body = "Nom: $name\n";
     $email_body .= "E-mail: $mail\n\n";
     $email_body .= "Message:\n$message\n";
 
-    // Email headers
     $headers = "From: $mail\n";
     $headers .= "Reply-To: $mail\n";
     $headers .= "Content-Type: text/plain; charset=UTF-8\n";
 
-    // Send the email
     if (mail($to_email, $subject, $email_body, $headers)) {
         echo "Message envoyé avec succès!";
     } else {
--- mila_website/php/subscribe.php
+++ mila_website/php/subscribe.php
@@ -1,6 +1,5 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Récupérer et valider l'adresse e-mail
     $email = filter_var(trim($_POST["mail"]), FILTER_SANITIZE_EMAIL);
 
     if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
@@ -8,10 +7,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         exit;
     }
 
-    // Chemin vers le fichier de stockage des e-mails
     $file = 'newsletter_subscribers.txt';
 
-    // Enregistrer l'adresse e-mail dans le fichier
     if (file_put_contents($file, $email . PHP_EOL, FILE_APPEND | LOCK_EX)) {
         echo "Vous êtes inscrit(e) à la newsletter avec succès!";
     } else {
